d83c35bdea4c6054eeea82cad0011e4cf37bdba1
Eric Liang <eric.liang@mail.utoronto.ca>
2020-04-14 01:49:16 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from 
+def main():
+  n = 0
+  player = 10
+  enemy = 20
+  board = genBoard()
+  print ("raw board is: (index=0 ... index=63):")
+  print (board)
+  print (" \nwhich will look like the following:")
+  print (printBoard(genBoard()))
+  print ("")
+  print (" Note 1: lower right hand square is WHITE")
+  print (" Note 2: two upper rows are for BLACK PIECES")
+  print (" Note 3: two lower rows are for WHITE PIECES")
+
+  while (n <= 20):
+      pos1 = int(input ("Enter a position of the piece that you want to move:"))
+      pos2 = int(input ("Enter a position that you wish to move to"))
+      P_move = [pos1,pos2]
+      print(P_move)
+      board = genNewBoard(board,P_move,player)
+      print(printBoard(board))
+      lol = chessPlayer(board,enemy)
+      board = genNewBoard(board,[lol[1][0],lol[1][1]],player)
+      print(printBoard(board))
+      n += 1
+      
+ main()
+
